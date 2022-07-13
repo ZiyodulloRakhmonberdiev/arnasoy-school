800a2f54f09fd76825adb5192693678ed183dcb6
Ziyodullo Rakhmonberdiev Web Developer <ziyodullo808@gmail.com>
2022-07-13 08:04:19 +0300

    all pages ready

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ let today = new Date()
 
 let date =
   today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear()
-
+let year = today.getFullYear()
+const currentYear = document.querySelector('.current-year')
 const navToggler = document.querySelector('.nav-toggler')
 const nav = document.querySelector('.nav')
 
 navToggler.addEventListener('click', () => {
   nav.classList.toggle('nav-active')
 })
+
+currentYear.innerHTML = year
